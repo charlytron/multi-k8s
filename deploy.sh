9d77dc26623d1f8514ba197a5430af32e47073ed
C Tompkins <celloviolinbass@yahoo.com>
2021-08-26 09:47:49 -0500

    ingress

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,17 @@
-docker build -t charlytron/multi-client -f ./client/Dockerfile ./client
-docker build -t charlytron/multi-server -f ./server/Dockerfile ./server
-docker build -t charlytron/multi-worker -f ./worker/Dockerfile ./worker
-docker push charlytron/multi-client
-docker push charlytron/multi-server
-docker push charlytron/multi-worker
+docker build -t charlytron/multi-client:latest -t charlytron/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t charlytron/multi-server:latest -t charlytron/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t charlytron/multi-worker:latest -t charlytron/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push charlytron/multi-client:latest
+docker push charlytron/multi-server:latest
+docker push charlytron/multi-worker:latest
+
+docker push charlytron/multi-client:$SHA
+docker push charlytron/multi-server:$SHA
+docker push charlytron/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server charlytron/multi-server
+kubectl set image deployments/server-deployment server=charlytron/multi-server:$SHA
+kubectl set image deployments/client-deployment client=charlytron/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=charlytron/multi-worker:$SHA
 
 
