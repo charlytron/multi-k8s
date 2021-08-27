fe98238e829aee21fb2048b379c092b0ba38f441
C Tompkins <celloviolinbass@yahoo.com>
2021-08-26 19:26:07 -0500

    Adjusted Version Number and Spelling

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fibonacci Calculator, Version Kubernetes
+            Fibonacci Calculator, Version 2.1
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
